fc37e135e9a3a95cc87f9c408c8777cecf9560f0
Tyler Dreher <drehertyler@gmail.com>
2018-04-28 23:53:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,6 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
-function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
 }
