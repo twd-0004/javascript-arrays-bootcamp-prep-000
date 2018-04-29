2eb2a8819bc2b61778f78d3f59564fe1f7ff4643
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 00:24:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array;
+  array2 = array.slice(1);
+  return array2;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
