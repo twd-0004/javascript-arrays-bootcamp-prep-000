1de85d0f94ff72ab2f2b1b9be4d74bafd7df6c43
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 00:21:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array = [...array, element];
+  array = array.push(element);
   return array
 }
 
